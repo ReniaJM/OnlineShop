b2143fcc037beb09397568a3236c00f25193b1d0
ReniaJM <prana861@gmail.com>
2019-04-27 20:28:27 +0200

    add mongoose

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://Renata:renia861@ds149146.mlab.com:49146/onlineshop',
+    port: 3030,
+    secret: 'Renia861'
+};
--- server/server.js
+++ server/server.js
@@ -3,11 +3,35 @@ const morgan = require('morgan');
 // HTTP request logger middleware for node.js
 const bodyPartser = require('body-parser');
 const mongoose =  require('mongoose');
+const cors = require('cors');
 
 const app = express();
+const config = require('./config');
+
+mongoose.connect(config.database,{ useNewUrlParser: true } ,err => {
+   if(err){
+       console.log(err);
+   }else{
+        console.log('connected to database');
+    }
+});
+
 app.use(bodyPartser.json());
-app.use(bodyPartser.urlencoded({extended:false}));
+app.use(bodyPartser.urlencoded({extended:true}));
 // jest na false ab ograniczyc czytanie innych formatow tj img
 app.use(morgan('dev'));
 // loguje wszystkie zapytania na terminalu
+app.use(cors());
+
+app.get('/', (req, res, next ) => {
+    res.json({
+        user:"jnfkjdfn"
+    })
+});
+
+app.listen(config.port, err =>{
+    console.log("serwer is working on port " + config.port)
+});
+
+
 
